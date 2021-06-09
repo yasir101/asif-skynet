b337141c516abd10ecb01bef2f98eb11e7653901
Shazim Khan <iamshazimkhan@gmail.com>
2021-06-09 14:36:11 +0500

    Integrate theme, bootstrap, jquery and haml

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,16 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "@fortawesome/fontawesome-free/js/all";
+
+require("bootstrap");
+
+// Add custom files here
+require("../packs/theme.min.js");
+
+$(document).on('turbolinks:load', function(){
+  // Custom logic here
+});
 
 Rails.start()
 Turbolinks.start()
