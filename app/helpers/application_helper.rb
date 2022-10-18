6c30309f7add1a709678c06db9a21c5bbd49083d
Yasir Amjad <iamyasiramjad@gmail.com>
2022-10-18 23:18:13 +0500

    sewtting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def extract_url(val)
     val.split(/\W/).last
   end
+  
+  def get_service(customer)
+    return 'N/A' if customer.nil?
+    
+    if customer&.service&.name == 'Internet'
+      "#{customer&.service&.name} / #{customer&.internet_type&.name}"
+    else
+       "Cable"
+    end
+  end
 end
