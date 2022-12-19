c658f0e398579ccfab10a413fc8195614ddecf2d
Yasir Amjad <iamyasiramjad@gmail.com>
2022-12-19 22:31:02 +0500

    fixed payment module

--- app/helpers/app/payments_helper.rb
+++ app/helpers/app/payments_helper.rb
@@ -2,6 +2,8 @@ module App::PaymentsHelper
   def convert_amount(payment)
     if payment.purpose == 'advance_return' || payment.purpose == 'loan_return'
       "<span class='text-danger'>-#{payment.amount}</span>".html_safe
+    else
+      "<span class='text-success'>#{payment.amount}</span>".html_safe  
     end
   end
 end
