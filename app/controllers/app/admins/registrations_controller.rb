cf1ec30de26f389445e8d1a3e5c07f163fe8a829
Hamza Naveed <officialhamxa@gmail.com>
2023-02-17 00:00:25 +0500

    Fixes

--- app/controllers/app/admins/registrations_controller.rb
+++ app/controllers/app/admins/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class App::Admins::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -10,9 +10,9 @@ class App::Admins::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # GET /resource/edit
   # def edit
@@ -41,9 +41,9 @@ class App::Admins::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
@@ -64,6 +64,6 @@ class App::Admins::RegistrationsController < Devise::RegistrationsController
 
   def sign_up_params
     # add custom parameter here
-    params.require(:admin).permit(:email, :password, :password_confirmation)
+    params.require(:admin).permit(:username, :email, :password, :password_confirmation)
   end
 end
